132dc68c9ee68058ba6915cbf48eaa3f053c54eb
João Vitor Lima <joaoheldlop9367@gmail.com>
2026-07-06 22:21:32 -0300

    EX 4 SOBRE DICIONARIO DE ALUNO

---
+++ AULA7_EX4.py
@@ -0,0 +1,12 @@
+ficha_aluno = {
+    "Nome": "JONH",
+    "Matricula": 35567,
+    "Curso": "Ads",
+    "Situação": "Ativa"
+}
+
+print(f"-- Ficha do Aluno(a): {ficha_aluno['Nome']} ---")
+print(f"Nome: {ficha_aluno['Nome']}")
+print(f"Matricula: {ficha_aluno['Matricula']}")
+print(f"Curso: {ficha_aluno['Curso']}")
+print(f"Situação: {ficha_aluno['Situação']}")
